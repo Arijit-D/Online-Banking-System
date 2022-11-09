29b97509bde67934f7c4dddd83611164b25dbb8c
Arijit Dhar <104313906+Arijit-D@users.noreply.github.com>
2022-11-10 01:19:31 +0530

    added

---
+++ Online Banking System/src/com/Application/Main.java
@@ -0,0 +1,72 @@
+package com.Application;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+    Scanner scanner = new Scanner(System.in);
+		
+	System.out.println(".......Welcome to ARD Bank.......");
+	System.out.println();
+	System.out.println("-------Plese Choose Options--------");
+	System.out.println("--------------------------------------");
+	
+	System.out.println("Press 1 for Login As Accountant");
+	System.out.println();
+	System.out.println("Press 2 for login As Employee");
+	System.out.println();
+	System.out.println("Press 3 for Exit");
+	
+	int key = scanner.nextInt();
+	
+	switch (key) {
+	case 1: {
+		
+		System.out.println("Enter Accountant Id");
+	    String accountant = scanner.next();
+	    
+	    System.out.println("Enter Accountant Password");
+	    String Password = scanner.next();
+	    
+	    if(accountant.equals("Admin") && Password.equals("Accountant")) {
+	    	
+	    	Accountant.admin();
+	    	
+	    }
+	    
+	    else 
+		{
+		System.out.println("Wrong Username And PassWord");
+		}
+	
+	    break;
+	    
+		
+	}
+   case 2: {
+//		Customer.login();
+	   
+	  
+		break;
+	    
+		
+	}
+   case 3: {
+		
+	   System.out.println("...... BYE......");
+	
+		
+		break;
+		
+	}
+	default:
+		throw new IllegalArgumentException("Unexpected value: " + key);
+	}
+		
+		
+	}
+
+}
---
+++ Online Banking System/src/com/dao/CustomerDaoImpl.java
@@ -0,0 +1,11 @@
+package com.dao;
+
+public class CustomerDaoImpl implements CustomerDao{
+
+	@Override
+	public String registerCustomer(String cname, String password, String email, int mobile_no, int balance) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
